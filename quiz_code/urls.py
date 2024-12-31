3ce4a0cb408f01050fb3f75a0b2f97f3cebafbb1
IA-LINK <jyc.kom@gmail.com>
2024-12-30 20:33:10 -0500

    update flask app

--- quiz_code/urls.py
+++ quiz_code/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for quiz_base project.
+URL configuration for quiz_code project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
