ffcc6d5d4bd133469e1e16fee03f78794db81c77
IA-LINK <jyc.kom@gmail.com>
2024-12-31 23:08:10 -0500

    Responsive, DynamicREST andAPI

--- quizz/views.py
+++ quizz/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Quiz, Question, Option, Result
 from django.contrib.auth.decorators import login_required, user_passes_test
 from rest_framework.response import Response
+from .forms import QuizForm, QuestionForm, OptionForm
 from rest_framework.views import APIView
 from .serializers import QuizSerializer
 from django.contrib import messages
@@ -100,7 +101,6 @@ def admin_quiz_list(request):
     quizz = Quiz.objects.all()
     return render(request, 'quizz/admin_quiz_list.html', {'quizz': quizz})
 
-from django.contrib.auth.decorators import login_required, user_passes_test
 
 @login_required
 @user_passes_test(lambda u: u.is_staff)  # Restrict to staff users
@@ -141,3 +141,30 @@ def take_quiz(request, quiz_id):
         return redirect('take_quiz', quiz_id=quiz.id)
 
     return render(request, 'quizz/take_quiz.html', {'quiz': quiz, 'question': question})
+
+def admin_add_quiz(request):
+    if request.method == 'POST':
+        quiz_form = QuizForm(request.POST)
+        if quiz_form.is_valid():
+            quiz_form.save()
+            return redirect('admin_quiz_list')
+    else:
+        quiz_form = QuizForm()
+    return render(request, 'admin_add_quiz.html', {'quiz_form': quiz_form})
+
+def admin_add_question(request, quiz_id):
+    quiz = Quiz.objects.get(id=quiz_id)
+    if request.method == 'POST':
+        question_form = QuestionForm(request.POST)
+        option_form = OptionForm(request.POST)
+        if question_form.is_valid() and option_form.is_valid():
+            question = question_form.save(commit=False)
+            question.quiz = quiz
+            question.save()
+            option_form.save(commit=False).question = question
+            option_form.save()
+            return redirect('admin_quiz_list')
+    else:
+        question_form = QuestionForm()
+        option_form = OptionForm()
+    return render(request, 'admin_add_question.html', {'question_form': question_form, 'option_form': option_form})
